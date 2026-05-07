3fdaa8a3741483182c8d6681a19d02960f3b7da6
Mon <monityme@gmail.com>
2026-05-07 13:40:12 +0200

    Route y componentes shad-cn
    Rutas del navegador que habrá en el Layout y algunos componentes necesarios de shad-cn

---
+++ src/pages/Layout.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+    
+</script>
+
+<template>
+    <div>
+        <header>
+            <!--Logo / Barra de navegación / Idiomas -->
+        </header>
+    </div>
+
+    <main>
+        <RouterView/>
+    </main>
+</template>
+
+<style scoped>
+
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,65 @@
 import Home from "@/pages/home/Home.vue";
 import { createRouter, createWebHashHistory } from "vue-router";
-
-
+import Layout from "@/pages/Layout.vue";
+import About from "@/pages/about/About.vue";
+import Programa from "@/pages/programa/Programa.vue";
+import Invitades from "@/pages/invitades/Invitades.vue";
+import Espacio from "@/pages/espacio/Espacio.vue";
+import FAQ from "@/pages/faq/FAQ.vue";
+import Entradas from "@/pages/entradas/Entradas.vue";
 
 export const router = createRouter({
   history: createWebHashHistory(import.meta.env.BASE_URL),
 
   routes: [
+    {
+      path: '/',
+      children: [
+        {
+          path: '', 
+          component: Layout,
+          children: [
+            {
+              path: '',
+              name: 'home',
+              component: Home
+            },
+            {
+              path: 'about',
+              name: 'about',
+              component: About
+            },
+            {
+              path: 'programa',
+              name: 'programa',
+              component: Programa
+            },
+            {
+              path: 'invitades',
+              name: 'invitades',
+              component: Invitades
+            },
+            {
+              path: 'espacio',
+              name: 'espacio',
+              component: Espacio
+            },
+            {
+              path: 'faq',
+              name: 'faq',
+              component: FAQ
+            },
+            {
+              path: 'entradas',
+              name: 'entradas',
+              component: Entradas
+            }
+          ]
+        }
+
+      ]
+    }
+    /*
     {
       path: '/',
       name: 'home',
@@ -16,5 +69,6 @@ export const router = createRouter({
       path: '/:patchMatch(.*)',
       redirect: '/'
     }
+      */
   ]
 })
